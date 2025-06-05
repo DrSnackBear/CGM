b2481ca71ab904f1aa839389e92ce273d673c4eb
Melina Felicia Gabriele Gebhardt <gebhardt.mel@musterschule.schule.paedml>
2025-06-05 12:32:14 +0200

    SpielerTabelle erstellt

--- CGM-Projekt.py
+++ CGM-Projekt.py
@@ -1,3 +1,13 @@
 import sqlite3
 connection = sqlite3.connect('SpielCGM.db')
 cursor = connection.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXITS spieler
+        id INTEGER PRIMARY KEY,
+        spieler_id INTEGER,
+        name TEXT,
+        fähigkeit TEXT,
+        gold INTEGER,
+        FOREIGN KEY(spieler_id) REFERNCES spieler(id)
+)''')
