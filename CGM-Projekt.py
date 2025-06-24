b89fe0ebcf5a66b0288be1181a70a08c6a4eae72
Melina Felicia Gabriele Gebhardt <gebhardt.mel@musterschule.schule.paedml>
2025-06-24 09:30:56 +0200

    neue Tabelle erweitert

--- CGM-Projekt.py
+++ CGM-Projekt.py
@@ -3,12 +3,15 @@ connection = sqlite3.connect('SpielCGM.db')
 cursor = connection.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXITS spieler
+    CREATE TABLE IF NOT EXITS Spielstand
+        id INTEGER PRIMARY KEY,
+        spieler_id INTEGER,
         name TEXT,
         score INTEGER,
+        FOREIGN KEY(spieler_id) REFERENCES spieler(id)
 )''')
 
-cursor.execute("INSERT INTO spieler (spieler_id, name, fähigkeit, gold VALUES (1, Georg, Bogenschießer, 3)")
-cursor.execute("INSERT INTO spieler (spieler_id, name, fähigkeit, gold VALUES (2, Melina, Magierin, 4)")
-cursor.execute("INSERT INTO spieler (spieler_id, name, fähigkeit, gold VALUES (3, Christin, Schwertkämpferin, 5)")
+cursor.execute("INSERT INTO Spielstand (spieler_id, name, score) VALUES (1, Spieler1, 0)")
+cursor.execute("INSERT INTO Spielstand (spieler_id, name, score) VALUES (2, Spieler2, 0)")
 connection.commit()
+
